dc9a4f660b7ea874ab1cda48440c43ab18cd1c26
Artur Bożek <arturhubert.bozek@gmail.com>
2023-07-21 18:25:14 +0200

    fix add card

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,7 @@ const reducer = (state, action) => {
     case 'ADD_COLUMN':
         return { ...state, columns: [...state.columns, { ...action.newColumn, id: shortid() }] };
     case 'ADD_CARD':
-        return { ...state, cards: [...state.cards, { ...action.newCard, id: shortid(), columnId: action.columnId }] };
+        return { ...state, cards: [...state.cards, { ...action.newCard, id: shortid() }] };
     default:
         return state;
     }
