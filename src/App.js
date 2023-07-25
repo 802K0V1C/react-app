623ec22c4c2cd6f4fb8db1cb6c51164e5423da9a
Artur Bożek <arturhubert.bozek@gmail.com>
2023-07-25 10:45:25 +0200

    add page not found

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home/Home';
 import About from './components/About/About';
 import Favorite from './components/Favorite/Favorite';
 import { Routes, Route,  } from 'react-router-dom';
+import NotFound from './components/PageNotFound/PageNotFound.js';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/favorite" element={<Favorite />} />
+      <Route path="*" element={<NotFound/>}/>
     </Routes>
   </Container>
 </div>
